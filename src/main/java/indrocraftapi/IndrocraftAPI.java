20b6600a41c31b85b060997057d3f99143888dc4
OMEN44 <huonswales@gmail.com>
2022-02-01 16:16:41 +1000

    removed the initiaes utils

--- src/main/java/indrocraftapi/IndrocraftAPI.java
+++ src/main/java/indrocraftapi/IndrocraftAPI.java
@@ -5,8 +5,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class IndrocraftAPI extends JavaPlugin {
 
-    public Utils utils = new Utils();
-
     public static void test() {
         Bukkit.getLogger().severe("static");
     }
